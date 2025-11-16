70abf877cadf36773c133f9a3c40db3f20a4be0e
Marwan Elmallah <72896243+Marwan-Elmallah@users.noreply.github.com>
2025-11-16 14:05:57 +0400

    Update CV download link

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -15,7 +15,7 @@ export function HeroSection() {
 
   const downloadCV = () => {
     window.open(
-      "https://drive.google.com/file/d/1cWyrHJ06mVTSY4UDf4vE6i-75S73-zcY/view?usp=sharing",
+      "https://drive.google.com/file/d/1rp6UmSF01brs3Vrg-RrD5ILIqy_subk3/view?usp=sharing",
       "_blank",
       "noopener,noreferrer"
     )
